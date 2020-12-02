11529a6773b887b52c818ac4fc429ea3288b9bc4
scrabill <shannon.crabill@gmail.com>
2020-12-02 14:47:17 -0500

    Brute force solution for day 2, part 1, checking number of times key appears is within policy range

---
+++ 2020/2.js
@@ -0,0 +1,44 @@
+let fs = require("fs");
+let text = fs.readFileSync("./inputs/2.txt").toString('utf-8');
+let allText = text.split("\n")
+const input = allText.map(string => parseInt(string, 10))
+
+const testPasswordDatabase = [
+    "1-3 a: abcde",
+    "1-3 b: cdefg",
+    "2-9 c: ccccccccc"
+]
+
+function partOne(database) {
+    let validPasswords = 0
+
+    database.forEach(line => {
+
+       let [policy, password] = line.split(": ")
+
+       let key = policy.charAt(policy.length - 1)
+       let [lowerNum, upperNum] = policy.split("-", 2)
+       upperNum = upperNum.split(" ",1)
+
+       if (password.includes(key)) {
+
+        let counter = 0
+
+            for (let index = 0; index <= password.length; index++) {
+                if (password.charAt(index) == key) {
+                    counter++
+                }
+                
+            }
+
+            if (counter >= lowerNum && counter <= upperNum) {
+                validPasswords++
+            }
+       }
+
+    });
+
+    return validPasswords
+}
+
+console.log(partOne(allText))
